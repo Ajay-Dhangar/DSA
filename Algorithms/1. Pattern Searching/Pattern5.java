ada2faa992ca0bd43540a06d6ac2abefb22c7517
Ajay Dhangar <99037494+Ajay-Dhangar@users.noreply.github.com>
2023-01-02 11:20:35 +0530

    ..

--- Algorithms/1. Pattern Searching/Pattern5.java
+++ Algorithms/1. Pattern Searching/Pattern5.java
@@ -7,5 +7,19 @@
 */
 
 public class Pattern5 {
-    
+  public static void main(String[] args) {
+    int n = 4;
+    // outer loop
+    for (int i = 0; i < n; i++) {
+      // first inner loop
+      for (int j = n - 1; j >= i; j--) {
+        System.out.print("  ");
+      }
+      // second inner loop
+      for (int j = 0; j <= i; j++) {
+        System.out.print("* ");
+      }
+      System.out.println();
+    }
+  }
 }
--- Algorithms/1. Pattern Searching/Pattern7.java
+++ Algorithms/1. Pattern Searching/Pattern7.java
@@ -7,5 +7,15 @@
     1
 */
 public class Pattern7 {
-    
+    public static void main(String[] args) {
+        int n=5;
+        // outer loop
+        for(int i=n; i>=1; i--){
+            // inner loop
+            for(int j=1; j<=i; j++){
+                System.out.print(j+ " ");
+            }
+            System.out.println();           
+        }
+    }
 }
